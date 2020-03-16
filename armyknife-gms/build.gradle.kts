468b49cb6f1fbb21427fbbc28509c627d5166164
eaglesakura <eagle.sakura@gmail.com>
2020-03-16 09:44:28 +0900

    chg: udpate dependencies

--- armyknife-gms/build.gradle.kts
+++ armyknife-gms/build.gradle.kts
@@ -3,23 +3,24 @@ apply(from = "../dsl/ktlint.gradle")
 apply(from = "../dsl/bintray.gradle")
 
 dependencies {
-    "api"("com.eaglesakura.armyknife.armyknife-jetpack:armyknife-jetpack:1.4.1")
+    "api"("com.eaglesakura.armyknife.armyknife-jetpack:armyknife-jetpack:1.4.9")
 
-    "implementation"("androidx.fragment:fragment:1.2.0-rc01")
+    "implementation"("androidx.fragment:fragment:1.3.0-alpha01")
 
     /**
      * Google Play Services
      */
-    "implementation"("com.google.android.gms:play-services-auth:17.0.0")
+    "compileOnly"("com.google.android.gms:play-services-base:17.1.0")
+    "compileOnly"("com.google.android.gms:play-services-auth:17.0.0")
 
     /**
      * Firebase
      */
-    "implementation"("com.google.firebase:firebase-core:17.2.1")
-    "implementation"("com.google.firebase:firebase-auth:19.1.0")
-    "implementation"("com.google.firebase:firebase-config:19.0.3")
-    "implementation"("com.google.firebase:firebase-iid:20.0.0")
-    "implementation"("com.google.firebase:firebase-firestore:21.2.1")
-    "implementation"("com.google.firebase:firebase-storage:19.1.0")
-    "implementation"("com.crashlytics.sdk.android:crashlytics:2.10.1")
+    "compileOnly"("com.google.firebase:firebase-core:17.2.3")
+    "compileOnly"("com.google.firebase:firebase-auth:19.2.0")
+    "compileOnly"("com.google.firebase:firebase-config:19.1.2")
+    "compileOnly"("com.google.firebase:firebase-iid:20.1.1")
+    "compileOnly"("com.google.firebase:firebase-firestore:21.4.1")
+    "compileOnly"("com.google.firebase:firebase-storage:19.1.1")
+    "compileOnly"("com.crashlytics.sdk.android:crashlytics:2.10.1")
 }
