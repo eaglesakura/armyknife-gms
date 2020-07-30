e097a0ed832e65ca1dcd39939a8abf3e99bf1b24
eaglesakura <eagle.sakura@gmail.com>
2020-07-30 10:16:35 +0900

    chg: update dependencies

--- armyknife-gms/build.gradle.kts
+++ armyknife-gms/build.gradle.kts
@@ -6,8 +6,8 @@ dependencies {
     "api"("com.eaglesakura.armyknife.armyknife-jetpack:armyknife-jetpack:1.4.9")
 
     "compileOnly"("androidx.annotation:annotation:1.1.0")
-    "compileOnly"("androidx.core:core:1.2.0")
-    "compileOnly"("androidx.core:core-ktx:1.2.0")
+    "compileOnly"("androidx.core:core:1.3.1")
+    "compileOnly"("androidx.core:core-ktx:1.3.1")
     "compileOnly"("androidx.appcompat:appcompat:1.1.0")
     "compileOnly"("androidx.appcompat:appcompat-resources:1.1.0")
     "compileOnly"("androidx.lifecycle:lifecycle-extensions:2.2.0")
@@ -18,31 +18,31 @@ dependencies {
     /**
      * Google Play Services
      */
-    "compileOnly"("com.google.android.gms:play-services-base:17.1.0")
-    "compileOnly"("com.google.android.gms:play-services-auth:17.0.0")
+    "compileOnly"("com.google.android.gms:play-services-base:17.3.0")
+    "compileOnly"("com.google.android.gms:play-services-auth:18.1.0")
 
     /**
      * Firebase
      */
-    "compileOnly"("com.google.firebase:firebase-core:17.2.3")
-    "compileOnly"("com.google.firebase:firebase-auth:19.3.0")
-    "compileOnly"("com.google.firebase:firebase-config:19.1.2")
-    "compileOnly"("com.google.firebase:firebase-iid:20.1.1")
-    "compileOnly"("com.google.firebase:firebase-firestore:21.4.1")
+    "compileOnly"("com.google.firebase:firebase-core:17.4.4")
+    "compileOnly"("com.google.firebase:firebase-auth:19.3.2")
+    "compileOnly"("com.google.firebase:firebase-config:19.2.0")
+    "compileOnly"("com.google.firebase:firebase-iid:20.2.3")
+    "compileOnly"("com.google.firebase:firebase-firestore:21.5.0")
     "compileOnly"("com.google.firebase:firebase-storage:19.1.1")
     "compileOnly"("com.crashlytics.sdk.android:crashlytics:2.10.1")
 
-    "testImplementation"("com.google.firebase:firebase-core:17.2.3")
-    "testImplementation"("com.google.firebase:firebase-auth:19.3.0")
-    "testImplementation"("com.google.firebase:firebase-config:19.1.2")
-    "testImplementation"("com.google.firebase:firebase-iid:20.1.1")
-    "testImplementation"("com.google.firebase:firebase-firestore:21.4.1")
+    "testImplementation"("com.google.firebase:firebase-core:17.4.4")
+    "testImplementation"("com.google.firebase:firebase-auth:19.3.2")
+    "testImplementation"("com.google.firebase:firebase-config:19.2.0")
+    "testImplementation"("com.google.firebase:firebase-iid:20.2.3")
+    "testImplementation"("com.google.firebase:firebase-firestore:21.5.0")
     "testImplementation"("com.google.firebase:firebase-storage:19.1.1")
 
-    "androidTestImplementation"("com.google.firebase:firebase-core:17.2.3")
-    "androidTestImplementation"("com.google.firebase:firebase-auth:19.3.0")
-    "androidTestImplementation"("com.google.firebase:firebase-config:19.1.2")
-    "androidTestImplementation"("com.google.firebase:firebase-iid:20.1.1")
-    "androidTestImplementation"("com.google.firebase:firebase-firestore:21.4.1")
+    "androidTestImplementation"("com.google.firebase:firebase-core:17.4.4")
+    "androidTestImplementation"("com.google.firebase:firebase-auth:19.3.2")
+    "androidTestImplementation"("com.google.firebase:firebase-config:19.2.0")
+    "androidTestImplementation"("com.google.firebase:firebase-iid:20.2.3")
+    "androidTestImplementation"("com.google.firebase:firebase-firestore:21.5.0")
     "androidTestImplementation"("com.google.firebase:firebase-storage:19.1.1")
 }
--- configure.gradle.kts
+++ configure.gradle.kts
@@ -1,6 +1,6 @@
 import java.nio.charset.Charset
 
-extra["base_version"] = "1.4"
+extra["base_version"] = "1.5"
 extra["artifact_name"] = project.name
 extra["artifact_group"] = "com.eaglesakura.armyknife.${extra["artifact_name"]}"
 extra["bintray_user"] = "eaglesakura"
