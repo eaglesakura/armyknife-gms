d313789fed3e5f3564f6f5e769e9b350937a9a66
eaglesakura <eagle.sakura@gmail.com>
2020-10-19 22:54:33 +0900

    mod: update dependencies

--- armyknife-gms/build.gradle.kts
+++ armyknife-gms/build.gradle.kts
@@ -7,8 +7,8 @@ dependencies {
     "api"("com.eaglesakura.armyknife.armyknife-jetpack:armyknife-jetpack:1.4.11")
 
     "compileOnly"("androidx.annotation:annotation:1.1.0")
-    "compileOnly"("androidx.core:core:1.3.1")
-    "compileOnly"("androidx.core:core-ktx:1.3.1")
+    "compileOnly"("androidx.core:core:1.3.2")
+    "compileOnly"("androidx.core:core-ktx:1.3.2")
     "compileOnly"("androidx.appcompat:appcompat:1.2.0")
     "compileOnly"("androidx.appcompat:appcompat-resources:1.2.0")
     "compileOnly"("androidx.lifecycle:lifecycle-extensions:2.2.0")
@@ -25,26 +25,26 @@ dependencies {
     /**
      * Firebase
      */
-    "compileOnly"("com.google.firebase:firebase-core:17.5.0")
+    "compileOnly"("com.google.firebase:firebase-core:17.5.1")
     "compileOnly"("com.google.firebase:firebase-auth:19.4.0")
     "compileOnly"("com.google.firebase:firebase-config:19.2.0")
-    "compileOnly"("com.google.firebase:firebase-iid:20.2.4")
-    "compileOnly"("com.google.firebase:firebase-firestore:21.6.0")
+    "compileOnly"("com.google.firebase:firebase-iid:20.3.0")
+    "compileOnly"("com.google.firebase:firebase-firestore:21.7.1")
     "compileOnly"("com.google.firebase:firebase-storage:19.2.0")
     "compileOnly"("com.google.firebase:firebase-storage:19.2.0")
-    "compileOnly"("com.google.firebase:firebase-crashlytics:17.2.1")
+    "compileOnly"("com.google.firebase:firebase-crashlytics:17.2.2")
 
-    "testImplementation"("com.google.firebase:firebase-core:17.5.0")
+    "testImplementation"("com.google.firebase:firebase-core:17.5.1")
     "testImplementation"("com.google.firebase:firebase-auth:19.4.0")
     "testImplementation"("com.google.firebase:firebase-config:19.2.0")
-    "testImplementation"("com.google.firebase:firebase-iid:20.2.4")
-    "testImplementation"("com.google.firebase:firebase-firestore:21.6.0")
+    "testImplementation"("com.google.firebase:firebase-iid:20.3.0")
+    "testImplementation"("com.google.firebase:firebase-firestore:21.7.1")
     "testImplementation"("com.google.firebase:firebase-storage:19.2.0")
 
-    "androidTestImplementation"("com.google.firebase:firebase-core:17.5.0")
+    "androidTestImplementation"("com.google.firebase:firebase-core:17.5.1")
     "androidTestImplementation"("com.google.firebase:firebase-auth:19.4.0")
     "androidTestImplementation"("com.google.firebase:firebase-config:19.2.0")
-    "androidTestImplementation"("com.google.firebase:firebase-iid:20.2.4")
-    "androidTestImplementation"("com.google.firebase:firebase-firestore:21.6.0")
+    "androidTestImplementation"("com.google.firebase:firebase-iid:20.3.0")
+    "androidTestImplementation"("com.google.firebase:firebase-firestore:21.7.1")
     "androidTestImplementation"("com.google.firebase:firebase-storage:19.2.0")
 }
